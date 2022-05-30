9060397d632eb40ae8aed4243611651a534ec3a0
Ryan Saldaen <ryan.saldaen@gmail.com>
2022-05-30 03:28:29 -0700

    add javascript, need to fix icons

---
+++ client/src/js/database.js
@@ -0,0 +1,35 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const textDb = await openDB('jate', 1);
+  const tx = textDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ jate: content});
+  const result = await request;
+  console.log('Text added', result);
+};
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET FROM THE DATABASE');
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result;
+};
+
+initdb();
